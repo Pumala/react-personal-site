8cff019b6f66bd1e42ef5061839e5ec46bb10f22
Carolyn Lam <clamclam@Carolyns-MacBook-Pro.local>
2020-04-29 12:40:12 -0400

    revert

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -5,7 +5,6 @@ import githubImg from '../../images/github.png';
 import codesandboxImg from '../../images/codesandbox.png';
 import Header from '../Header/Header';
 import Quote from '../Quote/Quote';
-import { Link } from 'react-router-dom';
 
 const Contact = () => {
     return (
@@ -15,19 +14,19 @@ const Contact = () => {
                 <Quote />
                 <ul>
                     <li>
-                        <Link target="_blank" rel="noopener noreferrer" to="https://www.linkedin.com/in/carolyn-lam-9b719b128/">
+                        <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/carolyn-lam-9b719b128/">
                             <img src={linkedInImg} alt="linkedin icon" />
-                        </Link>
+                        </a>
                     </li>
                     <li>
-                        <Link target="_blank" rel="noopener noreferrer" to="https://github.com/Pumala">
+                        <a target="_blank" rel="noopener noreferrer" href="https://github.com/Pumala">
                             <img src={githubImg} alt="github icon" />
-                        </Link>
+                        </a>
                     </li>
                     <li>
-                        <Link target="_blank" rel="noopener noreferrer" to="https://codesandbox.io/u/Pumala">
+                        <a target="_blank" rel="noopener noreferrer" href="https://codesandbox.io/u/Pumala">
                             <img src={codesandboxImg} alt="codesandbox icon" />
-                        </Link>
+                        </a>
                     </li>
                 </ul>
             </div>
